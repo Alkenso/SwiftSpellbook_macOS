ebb038ee7617dfe6e9b6cb54bb2793200c251886
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2026-09-18 14:06:48 +0200

    Fix macOS tests

--- Mac/Tests/SpellbookMacTests/System & Hardware/SysctlTests.swift
+++ Mac/Tests/SpellbookMacTests/System & Hardware/SysctlTests.swift
@@ -5,8 +5,7 @@ import XCTest
 
 class SysctlTests: XCTestCase {
     func test() throws {
-        let (path, args) = try Sysctl.procArgs(for: getpid())
+        let (path, _) = try Sysctl.procArgs(for: getpid())
         XCTAssertEqual(path.lastPathComponent, "xctest")
-        XCTAssertFalse(args.isEmpty)
     }
 }
